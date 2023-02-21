279eb51d8d9283bd953a02eeadcd1cfaacb3d255
Fabian Heilig <fabian68.heilig@gmail.com>
2023-02-21 11:46:14 +0100

    profile picture and homepage background

--- src/sections/HomePage.jsx
+++ src/sections/HomePage.jsx
@@ -1,25 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, Pressable } from 'react-native';
+import {StyleSheet, Text, Pressable, Image, Dimensions} from 'react-native';
 import {useCounterSetter, useCounterValue} from "../providers/GameProvider";
-
+import {Video} from "expo-av";
+import React from "react";
 export function HomePage() {
     const counter = useCounterValue();
     const setCounter = useCounterSetter();
+    const videoRef = React.useRef(null);
     const onPress = () => {
         setCounter(counter + 1)
     }
 
     return (
         <Pressable style={styles.container} onPress={onPress}>
-            <Text>lignes de code : {counter}</Text>
+            <Video
+                source={require("../../assets/video1.mp4")}
+                style={styles.backgroundVideo}
+                muted={true}
+                repeat={true}
+                resizeMode={"cover"}
+                rate={1.0}
+                ignoreSilentSwitch={"obey"}
+                shouldPlay={true}
+                isLooping={true}
+            />
+            <Text style={{color: 'white'}}>lignes de code : {counter}</Text>
             <StatusBar style="auto" />
         </Pressable>
     );
 }
+const { height } = Dimensions.get("window");
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'lightblue',
         alignItems: 'center',
+        color: 'white'
     },
+    backgroundVideo: {
+        height: height,
+        position: "absolute",
+        top: 0,
+        left: 0,
+        alignItems: "stretch",
+        bottom: 0,
+        right: 0
+    }
 });
